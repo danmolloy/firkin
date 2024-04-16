c50e76a691d702a7f48f6784e9c6ba40b23d26c6
Daniel Molloy <danielmolloy_6@icloud.com>
2024-04-16 22:14:47 +0100

    css changes

--- components/header.tsx
+++ components/header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   
 
   return (
-    <div data-testid="header-section" className={`fixed w-full font-display border-b border-gray-600 h-14 flex flex-row justify-between items-center px-4 mix-blend-difference backdrop-blur z-20`}>
+    <div data-testid="header-section" className={`fixed w-full font-display border-b border-gray-600 h-14 flex flex-row justify-between items-center px-4 text-white backdrop-blur z-20`}>
        <button onClick={() => animateScroll.scrollToTop({duration: 500, smooth: true})} className={` font-display  `}>The Big Firkin Band</button>
       <DonateButton />
     </div>
