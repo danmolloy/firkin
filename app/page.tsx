41210a0d5a8a9c22659258be74e5e372bcbf9aa4
Daniel Molloy <danielmolloy_6@icloud.com>
2024-12-15 13:57:50 +0000

    removed calendar

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,26 @@ import { DateTime } from "luxon";
 import NewsletterIndex from "./newsletter";
 
 async function getData() {
-  const API_KEY = process.env.API_KEY
-      const CALENDAR_ID = process.env.CALENDAR_ID
-      const res = await fetch(
-        `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`,
-        {cache: "no-store"}
-      );
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
+  try {
+    const API_KEY = process.env.API_KEY
+        const CALENDAR_ID = process.env.CALENDAR_ID
+        const res = await fetch(
+          `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`,
+          {cache: "no-store"}
+        );
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      return {items: []}
+      /* throw new Error('Failed to fetch data') */
+    }
+    console.log(res.body);
+    return res.json()
+  } catch (e) {
+    console.log(`Error: ${e}`);
     return {items: []}
-    /* throw new Error('Failed to fetch data') */
   }
  
-  return res.json()
 }
 
 export type Event = {
@@ -42,9 +48,9 @@ export default async function Home() {
   //const [showHeader, setShowHeader] = useState<boolean>(true)
   //const [showMenu, setShowMenu] = useState<boolean>(false)
   const data = await getData()
-  const calendar = data.items.sort((a:Event, b:Event) => 
+  const calendar: Event[] = []/* data.items.sort((a:Event, b:Event) => 
     new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime()).filter((i: Event) => 
-      DateTime.fromJSDate(new Date(i.start.dateTime)) >= DateTime.now())
+      DateTime.fromJSDate(new Date(i.start.dateTime)) >= DateTime.now()) */
 
   return (
     <main className="flex flex-col text-black bg-slate-50 " data-testid="main-page">
