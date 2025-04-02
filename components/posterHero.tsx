56c9ed81fad800f78a3cfa949487e859bc2ac2e2
Daniel Molloy <danielmolloy_6@icloud.com>
2025-04-02 11:24:32 +0100

    restyled

--- components/posterHero.tsx
+++ components/posterHero.tsx
@@ -14,24 +14,27 @@ export default function PosterHero(props: HeroProps) {
   return (
     <div className="flex flex-col items-center justify-between h-[100dvh] w-[100vw] ">
             <Image className=" mt-20 mx-2" alt="The Big Firkin Band: 17-piece jazz big band sensation!" src="/hero-text.png" height={494/2} width={1180/2}/>
-            <div className="flex flex-row">
-            <p className="font-title w-[50vw] lg:ml-[16vw] text-blue-950 backdrop-blur-sm text-xl md:text-3xl px-2 font-bold  self-center">Live at the Fox & Firkin 2nd Sunday of the month 3:30pm-6pm!</p>
-            <div className="flex flex-col">
+            <div className="flex flex-row ">
+              <div className="font-title backdrop-blur-sm lg:ml-[16vw] text-blue-950  text-xl md:text-2xl px-2 font-bold  self-center">
+                <p>Live at The Fox & Firkin</p>
+                <p>Lewisham High Street</p>
+                <p>Second Sunday of the month</p>
+                <p>3.30-6pm</p>
+          <Link data-testid="insta-link" href="https://www.instagram.com/thebigfirkinband/" target="_blank"  title="Firkin Band Instagram" className="flex flex-row items-center self-end mt-4">
+            <FaInstagram />
+            <p className="text-lg mx-2 font-title">Follow us</p>
+          </Link>
+              </div>
             {nextGig !== undefined && 
-              <Link data-testid="next-gig" href={'/#calendar'} className="border-2 border-blue-950 rounded mx-4 hover:cursor-pointer hover:text-blue-900 text-blue-900 font-announce text-2xl md:text-3xl  lg:mr-[10vw] self-end px-2">
+            <div className="flex flex-col ">
+              <Link data-testid="next-gig" href={'/#calendar'} className="border shadow border-blue-950 rounded mx-4 hover:cursor-pointer hover:text-blue-900 text-blue-900 font-announce text-2xl md:text-3xl  lg:mr-[10vw] self-end px-2">
               <h2 className="">Next gig:</h2>
               <p className=" " data-testid="start-time">{DateTime.fromJSDate(new Date(nextGig.start.dateTime)).setZone('Europe/London').toFormat("dd LLL yyyy")}</p>
               <div className="flex flex-row items-center">
               <p data-testid="start-time">{DateTime.fromJSDate(new Date(nextGig.start.dateTime)).setZone('Europe/London').toFormat("h:mm a")} @ {nextGig.location ? nextGig.location.split(",")[0] : "Venue TBC"}</p>
                 </div>
-              </Link>}
-              <div className="flex flex-row  lg:mr-[20vw] mb-1 mt-1 md:mt-2 self-end text-blue-900">
-          <Link data-testid="insta-link" href="https://www.instagram.com/thebigfirkinband/" target="_blank"  title="Firkin Band Instagram" className="flex flex-row items-center ">
-        <FaInstagram />
-        <p className="text-lg mx-2 font-title">Follow us</p>
-      </Link>
-      </div>
-      </div>
+              </Link>
+      </div>}
 
           </div>
             <Image className="" src="/band-members.png" alt="The Big Firkin Band" height={298} width={716} />
